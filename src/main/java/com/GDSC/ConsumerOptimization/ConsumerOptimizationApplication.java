d2490a18bd2032545eb1f5e7bb1c66c2d9d308ea
BaverBayhan <bayhanbaver8@gmail.com>
2023-02-27 13:09:13 +0300

    Project init

---
+++ src/main/java/com/GDSC/ConsumerOptimization/ConsumerOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.GDSC.ConsumerOptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerOptimizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GDSC/ConsumerOptimization/ConsumerOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GDSC.ConsumerOptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

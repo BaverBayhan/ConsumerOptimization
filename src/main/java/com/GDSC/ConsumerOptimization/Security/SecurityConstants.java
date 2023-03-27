5ba168dda232f57ae094a4f861ea829003ec8cec
Baver Bayhan <110925866+BaverBayhan@users.noreply.github.com>
2023-03-27 12:07:32 +0300

    Update SecurityConstants.java

--- src/main/java/com/GDSC/ConsumerOptimization/Security/SecurityConstants.java
+++ src/main/java/com/GDSC/ConsumerOptimization/Security/SecurityConstants.java
@@ -2,5 +2,5 @@ package com.GDSC.ConsumerOptimization.Security;
 
 public class SecurityConstants {
     public static final long EXPIRATION_DATE = 700000;
-    public static final String SECRET_KEY= "SOME_SECRET_KEY";
+    public static final String SECRET_KEY= "GOC-SPX-exifK9TbmTBlELx2K_rqAPP4Eoo9";
 }
